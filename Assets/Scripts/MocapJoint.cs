83185b07508017f788d6088e24f2a03180515d82
Nick Fox-Gieg <nick@fox-gieg.com>
2019-08-05 19:36:31 -0400

    testing body and hands

---
+++ Assets/Scripts/MocapJoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MocapJoint : MonoBehaviour {
+
+    public Transform target;
+    public Renderer ren;
+    public float scale = 1f;
+    public bool debug = true;
+
+    private TrailRenderer trailRen;
+
+    private void Awake() {
+        trailRen = ren.gameObject.GetComponent<TrailRenderer>();
+    }
+
+    private void Start() {
+        setDebug(debug, scale);
+    }
+
+    private void Update() {
+        transform.position = target.position;
+    }
+
+    public void setDebug(bool _debug, float _scale) {
+        debug = _debug;
+        scale = _scale;
+
+        ren.enabled = debug;
+        trailRen.widthMultiplier = scale;
+        ren.transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+}
---
+++ Assets/Scripts/MocapVisualizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MocapVisualizer : MonoBehaviour {
+
+    public MocapJoint prefabJoint;
+    public bool debug = true;
+    public float scale = 1f;
+
+    [HideInInspector] public List<Transform> allTransforms = new List<Transform>();
+    [HideInInspector] public List<MocapJoint> joints;
+
+    private void Start() {
+        getTransformsFromHierarchy(transform);
+
+        foreach (Transform child in allTransforms) { 
+            MocapJoint joint = Instantiate(prefabJoint, Vector3.zero, Quaternion.identity).GetComponent<MocapJoint>();
+            joint.setDebug(debug, scale);
+            joint.target = child;
+            joints.Add(joint);
+        }
+    }
+
+    public void getTransformsFromHierarchy(Transform parent) {
+        foreach (Transform child in parent) {
+            allTransforms.Add(child);
+            getTransformsFromHierarchy(child);
+        }
+    }
+
+}
