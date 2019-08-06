ae8fba25be3d52203d2fecd0281401273ee6d1a1
Nick Fox-Gieg <nick@fox-gieg.com>
2019-08-06 15:19:48 -0400

    new osc

--- Assets/Scripts/MocapJointOsc.cs
+++ Assets/Scripts/MocapJointOsc.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MocapJointOsc : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class MocapJointOsc : MonoBehaviour {
+
+    private void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Update() {
         
     }
+
 }
--- Assets/Scripts/MocapOscHandler.cs
+++ Assets/Scripts/MocapOscHandler.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MocapOscHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class MocapOscHandler : MonoBehaviour {
+
+    private void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Update() {
         
     }
+
 }
