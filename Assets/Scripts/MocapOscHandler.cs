10bfcd19fe7d9fc44a54936aa6089c80754d8da0
Nick Fox-Gieg <nick@fox-gieg.com>
2019-08-08 11:24:56 -0400

    testing isadora osc

--- Assets/Scripts/MocapOscHandler.cs
+++ Assets/Scripts/MocapOscHandler.cs
@@ -8,18 +8,23 @@ using UnityOSC;
 
 public class MocapOscHandler : MonoBehaviour {
 
+    public enum ChannelFormat { BONE_NAME, ISADORA };
+    public ChannelFormat channelFormat = ChannelFormat.BONE_NAME;
+
     public string outIP = "127.0.0.1";
     public int outPort = 9999;
     public List<MocapVisualizer> mocapVisualizers = new List<MocapVisualizer>();
 
     private OSCServer myServer;
+    private string clientName = "myClient";
 
     private void Start() {
         OSCHandler.Instance.Init();
-        OSCHandler.Instance.CreateClient("myClient", IPAddress.Parse(outIP), outPort);
+        OSCHandler.Instance.CreateClient(clientName, IPAddress.Parse(outIP), outPort);
     }
 
     private void Update() {
+        int isadoraCounter = 1;
         for (int i=0; i<mocapVisualizers.Count; i++) { 
             if (mocapVisualizers[i].ready) {
                 foreach (Transform child in mocapVisualizers[i].taggedTransforms) {
@@ -27,7 +32,19 @@ public class MocapOscHandler : MonoBehaviour {
                     msg.Add(child.position.x);
                     msg.Add(child.position.y);
                     msg.Add(child.position.z);
-                    OSCHandler.Instance.SendMessageToClient("myClient", "/" + child.name, msg);
+
+                    string channelName;
+                    switch (channelFormat) {
+                        case (ChannelFormat.ISADORA):
+                            channelName = "/isadora-multi/" + isadoraCounter;
+                            isadoraCounter++;
+                            break;
+                        default:
+                            channelName = "/" + child.name;
+                            break;
+                    }
+
+                    OSCHandler.Instance.SendMessageToClient(clientName, channelName, msg);
                 }
             }
         }
